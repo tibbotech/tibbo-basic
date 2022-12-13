3fcdefe86d2223e65bc0a83b5054579ca6b3a020
Jimmy Hu <jimmy.hu@tibbo.com>
2022-12-13 17:55:07 +0800

    update exports

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,5 @@
 export * as TibboBasicTranspiler from './TibboBasicTranspiler';
+export * as TibboBasicProjectTranspiler from './TibboBasicProjectTranspiler';
 export * as TibboBasicJavascriptCompiler from './TibboBasicJavascriptCompiler';
 export * as TibboBasicPreprocessor from './TibboBasicPreprocessor';
 export * as TibboBasicProjectParser from './TibboBasicProjectParser';
