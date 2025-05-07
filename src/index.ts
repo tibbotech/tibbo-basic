911bf5485566fa696371c7416edddf0c9e54e5ba
Jimmy Hu <jimmy.hu@tibbo.com>
2025-05-07 11:17:05 +0800

    remove old src folder

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-export * from './TibboBasicTranspiler';
-export * from './TibboBasicProjectTranspiler';
-export * from './TibboBasicJavascriptCompiler';
-export * from './TibboBasicPreprocessor';
-export * from './TibboBasicProjectParser';
-export * from './TibboBasicDocumentFormatter';
-export * from './types';
-
-
